fe4e5933c73d2dc5ccd1958fcb32f2654d073d30
Karrakunga <karrakunga@gmail.com>
2016-03-15 16:27:29 +0000

    Added interface example

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,11 @@
 import {Messages} from './classes/Messages';
+import {House} from './classes/House';
+import {Door} from './classes/Door';
 
 var greeter = new Messages.Greeter("Hello World");
 
-document.body.innerHTML = greeter.greet();
+var door = new Door('Red', 'Wood');
+var house = new House(door);
+
+document.body.innerHTML = `${greeter.greet()} <br /> ${house.GetDoorProperties()}`
 
--- spec/unit.tests.spec.ts
+++ spec/unit.tests.spec.ts
@@ -1,9 +1,31 @@
-import {Messages} from '../app/classes/Messages';
+/// <reference path="../typings/main.d.ts" />
 
-describe("Greeter Tests", () => {
-    var greeter = new Messages.Greeter("Hello World");
+import {Messages} from '../app/classes/Messages';
+import {IDoor} from '../app/classes/Door';
+import {House} from '../app/classes/House';
+
+describe('Greeter Tests', () => {
+    var greeter = new Messages.Greeter('Hello World');
     
-    it("should have a message of hello world", () => {
-        expect(greeter.greeting).toBe("Hello World")  
+    it('should have a message of hello world', () => {
+        expect(greeter.greeting).toBe('Hello World')  
     });
 })
+
+
+describe('House Tests', () => {
+    var door = jasmine.createSpyObj('door', ['GetColor', 'GetHandleType']);
+    door.GetColor.and.callFake(function(){
+        return 'Blue';
+    });
+    door.GetHandleType.and.callFake(() => {
+        return 'Metal';
+    })    
+    
+    var house = new House(door);
+    
+    it('should have the correct door color', () => {
+        expect(house.GetDoorProperties()).toBe('Color: Blue, Handle Type: Metal');
+    })
+});
+    
